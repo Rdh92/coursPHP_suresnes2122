6ae91f5fe17b56e43388385c6e44529437c0a36b
Redha TALAMINE <redha.talamine@colombbus.org>
2021-12-13 10:48:52 +0100

    suite cours php

--- 01-intro/01_introduction.php
+++ 01-intro/01_introduction.php
@@ -28,21 +28,49 @@
               <li>La syntaxe et les caractéristiques du langage PHP (v7)</li>
               <li>Les notions essentielles du langage SQL permettant la gestion d'une BDD et la réalisation des requêtes de base</li>
               <li>... et les moyens d'y accéder à l'aide de fonctions spécialisées de PHP (ou d'objet)</li>
+              <li>
+                <!-- mon 1er passage PHP et un "echo" -->
+                <?php echo "<strong>Mon 1er texte fait en PHP</strong>"; ?>
+              </li>
             </ul>
           </div>
           <!-- fin col -->
 
+          
           <div class="col-md-4 p-1 border border-warning">
             <h4>2/ Qu'est-ce que PHP </h4>
+            <p>PHP permet de créer des pages interactives; une page interactive permet à un visiteur de saisir des données personnelles qui sont ensuite transmises au serveur, où elles peuvent rester stockées dans une BDD pour être diffusées vers d'autres utilisateurs. Un utilisateur peut par exemple s'enregistrer et retrouver une page adaptée à ses besoins lors d'une visite ultérieure. Il peut aussi envoyer des emails et des fichiers sans avoir à passer par son logiciel de messagerie. En associant toutes ces caractéristiques, il est possible de créer aussi bien des sites de diffusuin et de collecte d'informations que des sites e-commerce, de rencontres ou des blogs</p>
           </div>
           <!-- fin col -->
 
           <div class="col-md-4 p-1 border border-danger">
           <h4>3/ Rappel sur les BDD </h4>
+          <p>Pour contenir la masse d'informations collectées, PHP s'appuie généralement sur une BDD, le plus souvent MySQL mais aussi SQLite et sur des serveurs Apache. PHP, MySQL et Apache forment le trio ultra dominant sur les serveurs Internet. Quand ce trio est associé à un serveur Linux on parle de LAMP. PHP est utilisé par les 3/4 des sites de la planète</p>
+          <p>Pour utiliser PHP sur un PC on utilisera XAMPP mais aussi Laragon, sur un sur Mac on privilégiera MAMP</p>
           </div>
           <!-- fin col -->
         </section>
         <!-- fin row  -->
+
+        <section class="row">
+          <div class="col-md-4 p-1 border border-danger">
+            <p>Avec le code suivant, crit dans un fichier nommé 02_infos.php placé sur le serveur d'évaluation on obtient toutes les infos sur le php exécuté dans ce srveur</p>
+            <code class="alert alert-success">
+              <?php <br>
+                phpinfo(); // ceci est un commentaire<br>
+              ?><br>
+            </code>
+            <a class="btn btn-secondary btn-sm mb-2" href="02_infos.php">PHP infos</a>
+          </div>
+          <!-- fin col  -->
+
+          <div class="col-md-4 p-1 border border-danger">
+            <?php echo "date(D/M/Y - H:m:s"); ?>
+          </div>
+          <div class="col-md-4 p-1 border border-danger">
+
+          </div>
+        </section>
     </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
